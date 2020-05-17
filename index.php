8349498df92d3efeab800ecc16dfabbd5437698d
Ovidiu Muresan <osilvium@gmail.com>
2020-05-17 20:04:07 +0200

    aside styles

--- index.php
+++ index.php
@@ -79,13 +79,20 @@ if ($con->connect_error)
 			<main class="col-sm-10"><?php include "vistas/".$uri.".php"; $con->close();?></main>
 			<aside class="col-sm-2">
 				<div class="widget">
-					<p>No te vaya sin firmar en el <a href="/contacto">Libro de Visitas</a></p>
+					<p>No te vaya sin firmar en el <br><a href="/contacto">Libro de Visitas</a></p>
 				</div>
-				<div class="widget"></div>
-				<div class="widget"></div>
+				<div class="widget">Como hacer un servidor web con una Rasberry Pi<br><a href="proyectos">AQUI</a></div>
 				<div class="widget">
-					<div class="date"><?php echo date("d m Y"); ?></div>
-					<div class="date"><?php echo date("H:i"); ?></div>
+					<a href="/doc/CV_Nicolás-Ayuso-Bernal.pdf" download><svg class="bi bi-file-earmark-arrow-down" width="5em" height="5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path d="M4 1h5v1H4a1 1 0 00-1 1v10a1 1 0 001 1h8a1 1 0 001-1V6h1v7a2 2 0 01-2 2H4a2 2 0 01-2-2V3a2 2 0 012-2z"/>
+						<path d="M9 4.5V1l5 5h-3.5A1.5 1.5 0 019 4.5z"/>
+						<path fill-rule="evenodd" d="M5.646 9.146a.5.5 0 01.708 0L8 10.793l1.646-1.647a.5.5 0 01.708.708l-2 2a.5.5 0 01-.708 0l-2-2a.5.5 0 010-.708z" clip-rule="evenodd"/>
+						<path fill-rule="evenodd" d="M8 6a.5.5 0 01.5.5v4a.5.5 0 01-1 0v-4A.5.5 0 018 6z" clip-rule="evenodd"/>
+					</svg>CURRICULUM</a>
+				</div>
+				<div class="widget">
+					<div class="date"><?= date("d m Y") ?></div>
+					<div class="date"><?= date("H:i") ?></div>
 					<div class="date">Eres el visitante numero: <?php echo rand(500,1000); ?></div>
 				</div>
 			</aside>

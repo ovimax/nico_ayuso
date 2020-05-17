6511acdf42a11e2afa75ae6084e8f7eeddf0d455
Ovidiu Muresan <osilvium@gmail.com>
2020-05-17 19:20:26 +0200

    min and max sensor values

--- vistas/sensor-temperatura.php
+++ vistas/sensor-temperatura.php
@@ -1,6 +1,6 @@
 <?php
 // Definimos la consulta para la base de datos
-$sql = "SELECT * FROM datos ORDER BY fecha DESC LIMIT 1"; // Seleccionamos de la base de datos el ultimo registro de la tabla datos
+$sql = "SELECT * FROM datos"; // Seleccionamos de la base de datos el ultimo registro de la tabla datos
 
 // Lanzamos la consulta y recogemos los datos en caso de que hubieran y la  consulta haya sido correcta
 $response = $con->query($sql);
@@ -12,12 +12,37 @@ if ($response && isset($response->num_rows) && $response->num_rows > 0){
 		array_push($results,$row);
 	}
 }
-?>
+$last = end($results); // obtenemos el ultimo registro
 
-<h1>SENSOR #<?= $results[0]["id"] ?></h1>
-<p><span>FECHA:</span> <?= $results[0]["fecha"] ?></p>
-<p><span>TEMPERATURA:</span> <?= $results[0]["temperatura"] ?> ºC</p>
-<p><span>HUMEDAD:</span> <?= $results[0]["humedad"] ?> %</p>
+$min_temperatura = min(array_column($results, 'temperatura'));
+$max_temperatura = max(array_column($results, 'temperatura'));
+
+$min_humedad = min(array_column($results, 'humedad'));
+$max_humedad = max(array_column($results, 'humedad'));
+
+$minT = array_filter($results, function($value) use ($min_temperatura){
+    return $value["temperatura"] == $min_temperatura;
+});
+$minT = array_shift($minT);
+
+$maxT = array_filter($results, function($value) use ($max_temperatura){
+    return $value["temperatura"] == $max_temperatura;
+});
+$maxT = array_shift($maxT);
+
+$minH = array_filter($results, function($value) use ($min_humedad){
+    return $value["humedad"] == $min_humedad;
+});
+$minH = array_shift($minH);
+
+$maxH = array_filter($results, function($value) use ($max_humedad){
+    return $value["humedad"] == $max_humedad;
+});
+$maxH = array_shift($maxH);
+
+?>
+<h2>SENSOR TEMPERATURA #<?= $last["id"] ?></h2>
+<p><span>FECHA:</span> <?= $last["fecha"] ?></p>
 
 <table class="table table-bordered">
 	<tr>
@@ -25,7 +50,66 @@ if ($response && isset($response->num_rows) && $response->num_rows > 0){
 		<th>HUMEDAD</th>
 	</tr>
 	<tr>
-		<td><?= $results[0]["temperatura"] ?> ºC</td>
-		<td><?= $results[0]["humedad"] ?> %</td>
+		<td><?= $last["temperatura"] ?> ºC</td>
+		<td><?= $last["humedad"] ?> %</td>
 	</tr>
 </table>
+
+<h3>Minimo temperatura #<?= $minT["id"] ?></h3>
+<p><span>FECHA:</span> <?= $minT["fecha"] ?></p>
+
+<table class="table table-bordered">
+	<tr>
+		<th>TEMPERATURA</th>
+		<th>HUMEDAD</th>
+	</tr>
+	<tr>
+		<td><?= $minT["temperatura"] ?> ºC</td>
+		<td><?= $minT["humedad"] ?> %</td>
+	</tr>
+</table>
+
+
+<h3>Maxima temperatura #<?= $maxT["id"] ?></h3>
+<p><span>FECHA:</span> <?= $maxT["fecha"] ?></p>
+
+<table class="table table-bordered">
+	<tr>
+		<th>TEMPERATURA</th>
+		<th>HUMEDAD</th>
+	</tr>
+	<tr>
+		<td><?= $maxT["temperatura"] ?> ºC</td>
+		<td><?= $maxT["humedad"] ?> %</td>
+	</tr>
+</table>
+
+<h3>Minimo humedad #<?= $minH["id"] ?></h3>
+<p><span>FECHA:</span> <?= $minH["fecha"] ?></p>
+
+<table class="table table-bordered">
+	<tr>
+		<th>TEMPERATURA</th>
+		<th>HUMEDAD</th>
+	</tr>
+	<tr>
+		<td><?= $minH["temperatura"] ?> ºC</td>
+		<td><?= $minH["humedad"] ?> %</td>
+	</tr>
+</table>
+
+<h3>Maxima humedad #<?= $maxH["id"] ?></h3>
+<p><span>FECHA:</span> <?= $maxH["fecha"] ?></p>
+
+<table class="table table-bordered">
+	<tr>
+		<th>TEMPERATURA</th>
+		<th>HUMEDAD</th>
+	</tr>
+	<tr>
+		<td><?= $maxH["temperatura"] ?> ºC</td>
+		<td><?= $maxH["humedad"] ?> %</td>
+	</tr>
+</table>
+
+
